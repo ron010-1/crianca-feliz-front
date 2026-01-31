146cde4aa82da39208eb049b6ff5b5c277081f7d
Frombolas <gugalopes.dev@gmail.com>
2026-01-31 15:22:56 -0300

    adcionando verificação do token na pagina de visitas

--- src/pages/Visitas/index.tsx
+++ src/pages/Visitas/index.tsx
@@ -1,7 +1,18 @@
 import TabelaVisitas from "../../components/tabela-visitas/TabelaVisitas";
 import Style from "./visita.module.css";
+import { useAppSelector } from "../../hooks/useAppSelector";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
 
 export default function VisitasPage() {
+  const navigate = useNavigate();
+  const token = useAppSelector((state) => state.auth.token);
+  
+  useEffect(() => {
+    if (!token) {
+      navigate("/login");
+    }
+  }, [token, navigate]);
   return (
     <div className={Style.page}>
       <TabelaVisitas />
